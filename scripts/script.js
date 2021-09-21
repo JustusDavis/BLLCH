a5045c0dc7db860c044da2358af81f39f97436a8
BLL?CH <90290633+JustusDavis@users.noreply.github.com>
2021-09-21 15:06:17 -0500

    initial upload
    quick upload

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+/*
+
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello YWeb!';
+
+----------------------------------------------------
+
+let print = document.getElementById("jsTest");
+
+var num;
+num = 4;
+let update = num * 4;
+print.textContent = update;
+
+*/
+
+let print = document.getElementById("jsTest");
+
+var num;
+num = 4;
+let update = num * 6;
+print.textContent = update;
